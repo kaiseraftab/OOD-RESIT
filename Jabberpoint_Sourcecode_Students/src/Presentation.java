43389d9e1052c3b742df0c9d1e4c1050e4602fc5
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2024-04-21 23:51:58 +0200

    remove comment

--- Jabberpoint_Sourcecode_Students/src/Presentation.java
+++ Jabberpoint_Sourcecode_Students/src/Presentation.java
@@ -1,18 +1,5 @@
 import java.util.ArrayList;
 
-
-/**
- * <p>Presentations keeps track of the slides in a presentation.</p>
- * <p>Only one instance of this class is available.</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
- */
-
 public class Presentation {
 	private String showTitle; //The title of the presentation
 	private ArrayList<Slide> showList = null; //An ArrayList with slides
